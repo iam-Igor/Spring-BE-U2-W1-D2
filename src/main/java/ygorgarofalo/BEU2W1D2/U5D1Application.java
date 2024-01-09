b515c3bfbc3c5f64b10ec23e4e638bcfce5346bd
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-09 13:59:40 +0100

    Initial commit

---
+++ src/main/java/ygorgarofalo/BEU2W1D2/U5D1Application.java
@@ -0,0 +1,25 @@
+package ygorgarofalo.BEU2W1D2;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ygorgarofalo.BEU2W1D2.entities.Menu;
+
+@SpringBootApplication
+@Slf4j
+public class U5D1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5D1Application.class, args);
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5D1Application.class);
+
+        Menu m = (Menu) ctx.getBean("menu");
+
+        m.printMenu();
+        ctx.close();
+    }
+
+}
---
+++ src/test/java/ygorgarofalo/BEU2W1D1/BeU2W1D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.BEU2W1D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W1D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
